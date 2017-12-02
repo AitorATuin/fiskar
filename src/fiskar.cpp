1789ca2079b25670bf47cc7bec921e3681a35ce0
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2017-12-02 23:16:07 +0100

    Add simple promp to read values

--- src/fiskar.cpp
+++ src/fiskar.cpp
@@ -60,10 +60,86 @@ void setup() {
     lamps_scheduler_create(lamp_timers, USED_LAMPS);
     lamps_scheduler_init(&lamps_scheduler);
     Serial.println("Ready ...");
+    Alarm.delay(1000);
+}
+
+void prompt_show_timer(registered_lamp_timer_T t) {
+    SerialPrintF("\n%d:%d[%d-%d]\n", t.hours, t.minutes, t.lamp_pin, t.mode);
+}
+
+void prompt_show_timer_by_index(uint8_t n) {
+    prompt_show_timer(lamps_scheduler.registered_timers[n]);
+}
+
+void prompt_show_next_timer() {
+    prompt_show_timer_by_index(lamps_scheduler.current_timer_index);
+}
+
+void prompt_show_error(const char *err_str) {
+    SerialPrintF("%s\n", err_str);
+}
+
+void prompt_show_lamp(uint8_t lamp) {
+    registered_lamp_timer_T t;
+    for (uint8_t i=0;i<NLAMPS*NTIMERS*2;i++) {
+        t = lamps_scheduler.registered_timers[i];
+        if (t.lamp_pin == lamp && t.mode != LAMP_DISABLED)
+            prompt_show_timer_by_index(i);
+    }
+}
+
+void prompt_show_current_time() {
+    registered_lamp_timer_T t;
+    get_current_time(&t);
+    prompt_show_timer(t);
+}
+
+void prompt_process_read_command(char subcmd) {
+    char lamp[1];
+    uint8_t lamp_n;
+    switch(subcmd) {
+        case 'l':
+            Serial.readBytes(lamp, 1);
+            if (lamp[0] >= '0' && lamp[0] <= '9') {
+                lamp_n = lamp[0] - '0';
+                prompt_show_lamp(lamp_n);
+            }
+            else
+                prompt_show_error("E");
+            break;
+        case 'n':
+            prompt_show_next_timer();
+            break;
+        case 'c':
+            prompt_show_current_time();
+            break;
+        default:
+            prompt_show_error("E");
+    }
+}
+
+void prompt_process_set_command(char subcmd) {
+}
+
+void prompt_process_command() {
+    char cmd[2];
+    Serial.readBytes(cmd, 2);
+    switch(cmd[0]) {
+        case 'r':
+            prompt_process_read_command(cmd[1]);
+            break;
+        case 's':
+            prompt_process_set_command(cmd[1]);
+            break;
+        default:
+            prompt_show_error("E");
+    }
 }
 
 void loop() {
-    Alarm.delay(1000);
+    if (Serial.available() > 0) {
+        prompt_process_command();
+    }
 }
 
 
